1642f5606fca18315e5f80a8e29151ecf1633df3
Shamanskyi Bogdan <mmg.shamanskyi.b@gmail.com>
2019-11-08 09:31:36 +0200

    Create form to save user file

--- app/Services/SaveFile.php
+++ app/Services/SaveFile.php
@@ -13,6 +13,7 @@ class SaveFile
         $file_name = rand() . '.' . $request->file('file')->getClientOriginalExtension();
         $file->name = $file_name;
         $file->user_id = Auth::id();
+        $file->comment = $request->comment;
         $request->file('file')->move(public_path(Auth::id().'_userFiles'), $file_name);
         $file->save();
     }
