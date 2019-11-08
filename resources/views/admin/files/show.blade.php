61ce10ef14a1f287d11e40022965ea054328467a
Shamanskyi Bogdan <mmg.shamanskyi.b@gmail.com>
2019-11-08 13:09:32 +0200

    Created view show for one user file

---
+++ resources/views/admin/files/show.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1 class="text-center mb-4">
+        File {{ $file->name }}
+    </h1>
+    @if ( $file->comment != null )
+        <h3 class="text-center">Comment:&nbsp;{{ $file->comment }}</h3>
+    @endif
+    <hr>
+    <div class="text-center">
+        <img src="{{$file_path}}" width="700" height="500">
+    </div>
+    <hr>
+    <div class="text-center m-4">
+        <a class="btn btn-primary" href="{{route('files.create')}}">
+            <i class="fa fa-plus-square" aria-hidden="true"></i>&nbsp;Add another one file
+        </a>
+    </div>
+@endsection
