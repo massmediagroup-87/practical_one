8b5f7ade542d26b61b81fb2430369f0614999997
Shamanskyi Bogdan <mmg.shamanskyi.b@gmail.com>
2019-11-11 19:18:44 +0200

    Added validation to the size image, max:5Mb

--- app/Http/Requests/FileStoreRequest.php
+++ app/Http/Requests/FileStoreRequest.php
@@ -24,7 +24,7 @@ class FileStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'file' => 'required|image|mimes:jpeg,png,bmp,gif,svg,jpg'
+            'file' => 'required|image|mimes:jpeg,png,bmp,gif,svg,jpg|size:max:5000'
         ];
     }
 }
