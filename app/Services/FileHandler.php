ff5001ecb3cf251d000ef28eac56e14904c02bb9
Shamanskyi Bogdan <mmg.shamanskyi.b@gmail.com>
2019-11-11 19:24:43 +0200

    Created class FileHandler for handler files

---
+++ app/Services/FileHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Services;
+
+use App\UserFile;
+use Illuminate\Support\Facades\Storage;
+
+class FileHandler
+{
+    public function save($userId, $getFile, $comment): bool
+    {
+        $file = new UserFile();
+        $file->user_id = $userId;
+        $file->comment = $comment;
+        $pathName = $this->storeFile($getFile);
+        $file->name = $this->getFileName($pathName);
+        return $file->save();
+    }
+
+    public function storeFile($file): string
+    {
+        return $file->store($this->directoryPath());
+    }
+
+    public function getFileName(string $pathName): string
+    {
+        $name = substr($pathName, strlen($this->directoryPath()) + 1);
+        return $name;
+    }
+
+    public function getFilePath(string $fileName): string
+    {
+        return "files/$fileName";
+    }
+
+    private function directoryPath(): string
+    {
+        return 'files';
+    }
+
+    public function delete(UserFile $file): bool
+    {
+        $file->delete();
+        return Storage::delete($this->getFilePath($file->name));
+    }
+}
