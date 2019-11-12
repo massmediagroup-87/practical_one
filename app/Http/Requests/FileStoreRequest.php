201b3eadf5c7dde4438c40db5e4e6b634a2aadc1
Shamanskyi Bogdan <mmg.shamanskyi.b@gmail.com>
2019-11-12 09:30:01 +0200

    Replace condition 'size:max:' on 'max:5120'

--- app/Http/Requests/FileStoreRequest.php
+++ app/Http/Requests/FileStoreRequest.php
@@ -24,7 +24,7 @@ class FileStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'file' => 'required|image|mimes:jpeg,png,bmp,gif,svg,jpg|size:max:5000'
+            'file' => 'required|image|mimes:jpeg,png,bmp,gif,svg,jpg|max:5120'
         ];
     }
 }
